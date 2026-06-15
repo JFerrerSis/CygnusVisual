776ba59c1dabe2bd5390def0d3d8298f7b1f22fc
jferrer <jorgeferrercohen@gmail.com>
2026-06-15 10:23:36 -0400

    feat: add vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,17 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-
+import vercel from '@astrojs/vercel'; // 1. Asegúrate de importar esto
 export default defineConfig({
   integrations: [tailwind()],
-  output: 'server', // 'server' es la opción correcta para API routes
+
+  // 'server' es la opción correcta para API routes
+  output: 'server',
+
   server: {
     host: true,
-    port: 4321
-  }
+    port: 4321,
+    adapter: vercel(),
+  },
+
+  adapter: vercel()
 });
